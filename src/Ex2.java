8feab3fcfda1ed45634d157284b6ea7f81327926
RafoManukyan1999 <rafomanukyan27@gmail.com>
2025-04-18 19:06:28 +0400

    Exam 2

--- src/Ex2.java
+++ src/Ex2.java
@@ -41,68 +41,36 @@ public class Ex2 {
 
 
     @Test
-    public void testCompareArticleTitle(){
+    public void testSearchFieldHasExpectedText() {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Skip')]"),
-                "Cannot find 'Skip' input",
+                "Cannot find 'Skip' button",
                 5
         );
 
-        waitForElementAndClick(
+        assertElementHasText(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
-                "Cannot find 'Search Wikipedia' input",
+                "Search Wikipedia",
+                "Search field text does not match expected value",
                 5
-        );
-
-        waitForElementAndSendKeys(
-                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
-                "Python",
-                "Cannot find search input",
-                5
-        );
-
-        waitForElementAndClick(
-                By.xpath("//android.widget.TextView[@resource-id=\"org.wikipedia:id/page_list_item_title\" and @text=\"Python (programming language)\"]"),
-                "Cannot find 'Python (programming language)' topic searching by 'Python'",
-                5
-        );
-
-        WebElement assertElementHasText = waitForElementPresent(
-                By.xpath("//android.view.View[@text=\"Python (programming language)\"]"),
-                "Cannot find article title",
-                10
-        );
-
-        String article_title = assertElementHasText.getAttribute("text");
-        Assert.assertEquals(
-                "We see unexpected title",
-                "Python (programming language)",
-                article_title
         );
     }
 
-
-
-    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds)
-    {
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds) {
         WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
         element.click();
         return element;
     }
 
-    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
-    {
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
         wait.withMessage(error_message + "\n");
-        return wait.until(
-                ExpectedConditions.presenceOfElementLocated(by)
-        );
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
-    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds)
-    {
+    private void assertElementHasText(By by, String expected_text, String error_message, long timeoutInSeconds) {
         WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
-        element.sendKeys(value);
-        return element;
+        String actual_text = element.getText();
+        Assert.assertEquals(error_message, expected_text, actual_text);
     }
 }
